4d3028f587e9e16e0b43f981983f4fd5cb2c6147
Nishchal <gautam.nishchal@gmail.com>
2018-05-14 11:34:22 +0700

    feat(auto_updater): add auto updater

--- updater/release.sh
+++ updater/release.sh
@@ -53,6 +53,7 @@ git add plugin
 git remote add deploy https://cyberhck:$GH_TOKEN@github.com/fossapps/PhpStorm-Phalcon-Autocomplete.git
 echo "git remote add deploy https://cyberhck:$GH_TOKEN@github.com/fossapps/PhpStorm-Phalcon-Autocomplete.git"
 git commit -m "feat(ide_stubs): updated plugins to $release"
+git log
 git tag $release
 git remote show deploy
 git push deploy master
