96e60e229898b9a66791951c11f8d6506584fedd
Nishchal <gautam.nishchal@gmail.com>
2018-05-14 11:01:00 +0700

    feat(auto_updater): add auto updater

---
+++ updater/release.sh
@@ -0,0 +1,54 @@
+npm run build
+release=`node ./dist/index.js`
+curDir=`pwd`
+mkdir -p /tmp/releases
+wget https://github.com/phalcon/ide-stubs/archive/$release.zip -O /tmp/releases/release.zip
+cd /tmp/releases/
+unzip -o /tmp/releases/release.zip
+cd $(ls -d */|head -n 1)/src/
+rm -Rf ${curDir}/../plugin/Phalcon/Phalcon
+mv Phalcon ${curDir}/../plugin/Phalcon/
+cd ${curDir}/../plugin/
+version_number=${release/v/}
+# update the xml file META-INF/plugin.xml
+template=`cat << EOF
+<idea-plugin version="2">
+  <id>cyberhck.github.idea.plugin.phalcon</id>
+  <name>Phalcon auto-complete</name>
+  <version>${version_number}</version>
+  <vendor email="gautam.nishchal@gmail.com" url="https://github.com/cyberhck">Nishchal Gautam</vendor>
+
+  <description><![CDATA[
+    Provides autocompletions for Phalcon framework
+  ]]></description>
+
+  <change-notes><![CDATA[
+    <h3>${release}</h3>
+    <ul>
+      <li>updated to phalcon ${release}</li>
+    </ul>
+  ]]>
+  </change-notes>
+
+  <idea-version since-build="163.3984"/>
+
+  <depends>com.intellij.modules.platform</depends>
+  <depends>com.jetbrains.php</depends>
+
+  <extensions defaultExtensionNs="com.jetbrains.php">
+    <libraryRoot id="library" path="/Phalcon/Phalcon" runtime="false"/>
+  </extensions>
+</idea-plugin>
+EOF
+`
+echo "$template" > META-INF/plugin.xml
+zip -r phpstorm-library-plugin.jar *
+
+
+cd ${curDir}/../
+git add plugin
+git commit -m "feat(ide_stubs): updated plugins to $release"
+git tag $release
+git push origin/master
+git push --tags
+# create a tag
---
+++ updater/src/index.ts
@@ -0,0 +1,45 @@
+const fetch = require("node-fetch");
+const plugin = "fossapps/PhpStorm-Phalcon-Autocomplete";
+const stubs = "phalcon/ide-stubs";
+const url = "https://api.github.com/repos";
+interface IReleaseInfo {
+    id: string;
+    url: string;
+    tag_name: string;
+}
+
+const getLatestReleaseId = (baseUrl: string, repo: string): Promise<IReleaseInfo> => {
+    const url = `${baseUrl}/${repo}/releases`;
+    return fetch(url).then((res: any) => res.json()).then((res: IReleaseInfo[]) => {
+        return {
+            id: res[0].id,
+            url: res[0].url,
+            tag_name: res[0].tag_name
+        }
+    }).catch((e: any) => {console.error(e); return;});
+};
+const pluginData = getLatestReleaseId(url, plugin);
+const stubsData = getLatestReleaseId(url, stubs);
+Promise.all([pluginData, stubsData]).then((results: IReleaseInfo[]) => {
+    const plugin = results[0];
+    const stubs = results[1];
+    performRelease(plugin, stubs)
+}).catch((e: any) => {console.error(e)});
+
+const performRelease = (plugin: IReleaseInfo, stubs: IReleaseInfo): void => {
+    if (plugin.tag_name === stubs.tag_name) {
+        return;
+    }
+    console.log(stubs.tag_name);
+    // console.log(`Clone IDE stubs inside /tmp/`);
+    // console.log(`Move IDE stubs into plugins`);
+    // console.log(`generate JAR`);
+    // console.log(`UPDATE plugin.xml`);
+    // console.log(`Commit`);
+    // console.log(`Tag`);
+    // console.log(`push`);
+    // console.log(`create release`);
+    // console.log(`create issue in jet brains repo`);
+
+};
+
