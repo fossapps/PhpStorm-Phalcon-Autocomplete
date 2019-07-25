fabe6240ffc881c3d398f8faf0c97d83bb89f6c2
Travis CI User <travis@example.org>
2019-07-25 09:31:21 +0000

    feat(ide_stubs): updated plugins to v4.0.0-beta.1

---
+++ plugin/Phalcon/Phalcon/mvc/micro/LazyLoader.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Phalcon\Mvc\Micro;
+
+/**
+ * Phalcon\Mvc\Micro\LazyLoader
+ *
+ * Lazy-Load of handlers for Mvc\Micro using auto-loading
+ */
+class LazyLoader
+{
+
+    protected $handler;
+
+
+    protected $definition;
+
+
+
+    public function getDefinition() {}
+
+    /**
+     * Phalcon\Mvc\Micro\LazyLoader constructor
+     *
+     * @param string $definition
+     */
+    public function __construct(string $definition) {}
+
+    /**
+     * Calling __call method
+     *
+     * @param string $method
+     * @param array $arguments
+     * @param \Phalcon\Mvc\Model\BinderInterface $modelBinder
+     * @return mixed
+     */
+    public function callMethod(string $method, $arguments, \Phalcon\Mvc\Model\BinderInterface $modelBinder = null) {}
+
+}
