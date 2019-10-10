e57b6d96c9bd844193f2c1a75d229ffbe98b62d7
Travis CI User <travis@example.org>
2019-10-10 10:10:09 +0000

    feat(ide_stubs): updated plugins to v4.0.0-rc.1

---
+++ plugin/Phalcon/Phalcon/filter/FilterFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Phalcon\Filter;
+
+class FilterFactory
+{
+
+    /**
+     * Returns a Locator object with all the helpers defined in anonynous
+     * functions
+     *
+     * @return LocatorInterface
+     */
+    public function newInstance(): LocatorInterface
+    {
+    }
+
+    /**
+     * @return array
+     */
+    protected function getAdapters(): array
+    {
+    }
+}
