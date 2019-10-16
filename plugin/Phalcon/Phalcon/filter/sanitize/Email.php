103c3dbb214e7950b6b059db1f5a0ac109abfba9
Travis CI User <travis@example.org>
2019-10-16 10:14:28 +0000

    feat(ide_stubs): updated plugins to v4.0.0-rc.1

---
+++ plugin/Phalcon/Phalcon/filter/sanitize/Email.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Phalcon\Filter\Sanitize;
+
+/**
+ * Phalcon\Filter\Sanitize\Email
+ *
+ * Sanitizes an email string
+ */
+class Email
+{
+
+    /**
+     * @param mixed $input
+     */
+    public function __invoke($input)
+    {
+    }
+}
